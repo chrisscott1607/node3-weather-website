5194f96e128b58b47f4d6916ce1c9095f209cc54
Christopher Scott <christopherscott@Christophers-MacBook-Pro.local>
2020-06-03 18:50:55 -0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./Utils/forecast')
 const geocode = require('./Utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // console.log(__dirname)
 // console.log(__filename)
@@ -100,6 +101,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port .' + port)
 })
